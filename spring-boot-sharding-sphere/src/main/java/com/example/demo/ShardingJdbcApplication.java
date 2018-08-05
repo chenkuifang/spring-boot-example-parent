70ae7e590f18d1bcb2361c7e17c7c54672bd6373
chenkuifang <314287696@qq.com>
2018-08-05 21:49:32 +0800

    update

--- spring-boot-sharding-sphere/src/main/java/com/example/demo/ShardingJdbcApplication.java
+++ spring-boot-sharding-sphere/src/main/java/com/example/demo/ShardingJdbcApplication.java
@@ -10,9 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version V1.0
  */
 @SpringBootApplication
-public class MyApplication {
+public class ShardingJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(ShardingJdbcApplication.class, args);
     }
 }
---
+++ spring-boot-sharding-sphere/src/main/java/com/example/demo/entity/Goods.java
@@ -0,0 +1,31 @@
+
+
+package com.example.demo.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * 商品信息实体类
+ *
+ * @author ：QuiFar
+ */
+@Data
+public class Goods implements Serializable {
+	private static final long serialVersionUID = 903598773132953830L;
+	
+	private Integer id; 		/*id*/
+    private String goodsNum; 		/*商品编码*/
+    private String goodsName; 		/*商品名称*/
+    private BigDecimal salePrice; 		/*销售价格*/
+    private BigDecimal discountPrice; 		/*折后价格*/
+    private Integer stock; 		/*库存*/
+    private Integer saleCount; 		/*销量*/
+    private Integer status; 		/*状态  1:正常, 0:下架*/
+}
