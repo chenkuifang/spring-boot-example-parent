8b4fcf864041c3725925626e4771055bf47cc089
chenkuifang <314287696@qq.com>
2018-12-17 10:45:42 +0800

    创建线程方式

--- spring-boot-java8new/src/test/java/com/example/springbootjava8new/juc/ExecutorsTest.java
+++ spring-boot-java8new/src/test/java/com/example/springbootjava8new/juc/ExecutorsTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import java.util.concurrent.*;
 
 /**
+ * 复习：创建线程方式有4种
+ * 1.继承Thread类，-->重新run() --> 启动：new MyThread().start()
+ * 2.实现Runnable接口,--->重写run()--->启动：new Thread(new MyRunnable()).start
+ * 3.实现Callable接口(配合ExecutorService使用)，--->重写call()--->创建线程池：ExecutorService executorService = Executors.newFixedThreadPool(5);
+ * -->  Future result = executorService.sumbit(new MyCallable()); -->result.get() 获取返回值
+ * 4. 当然还可以使用JDK提供的线程池创建方式，一般使用Executors工具类。
+ * <p>
+ * <p>
  * Executors 工具类测试
  * 什么是任务：
  * 实现Callable接口或Runnable接口的类，其实例就可以成为一个任务提交给ExecutorService去执行,这样的好处是任务和执行解耦。
@@ -36,15 +44,18 @@ public class ExecutorsTest {
     class Task implements Runnable {
         @Override
         public void run() {
-            System.err.println("执行...");
+            System.err.println("running...");
         }
     }
 
+    /**
+     * 创建一个指定大小的线程池，并提交一个有返回的任务
+     */
     @Test
     public void test2() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Future submit = executorService.submit(new CallBackTask());
-        System.err.println(submit.get());
+        Future result = executorService.submit(new CallBackTask());
+        System.err.println(result.get());
     }
 
     class CallBackTask implements Callable<String> {
